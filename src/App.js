dc0caf274cbafd85439259a9d171f05ef86268b1
unknown <rnrudxo2872@naver.com>
2021-07-07 23:01:08 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const UserInput = (initValue, isValid) => {
+  const [value, setValue] = useState(initValue)
+
+  const onChange = (event) => {
+    const {target} = event;
+
+    const curValue = target.value;
+    let isExec = false
+    if(typeof isValid === "function"){
+      isExec = isValid(curValue);
+    }
+    if(isExec){
+      setValue(curValue);
+    }
+  }
+
+  return{value,onChange}
+}
+
 function App() {
+  const isValid = (value) => value.length < 5;
+  const nameState = UserInput("now",isValid)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h1>UseState - Input</h1>
+          <input {...nameState}/>
       </header>
     </div>
   );
